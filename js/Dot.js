955da524d6e27fd06f773144951ece8f658724aa
Maria Nattestad <mnattestad@dnanexus.com>
2017-10-19 17:10:39 -0700

    Replace canvas with SVG for all except alignments

--- js/Dot.js
+++ js/Dot.js
@@ -11,24 +11,33 @@ var DotPlot = function(element, config) {
 	validateConfig(config);
 
 	this.config = config;
-	
-	// Set up config variables
-	this.height = config.height;
-	this.width = config.width;
 	this.data = undefined;
 
-	this.settings = {
-		padding: {left: 120, bottom: 100}
-	}
-
 	this.state = {
+		layout: {
+			whole: {height: config.height, width: config.width},
+			inner: {height: null, width: null},
+
+		},
 		all_refs: null,
 		all_queries: null,
 		selected_refs: null,
 		selected_queries: null,
-		annotation_tracks: [],
 		data_by_chromosome: {},
 	};
+
+	this.k = { x: "ref", y: "query" };
+
+
+	// Set up permanent DOM elements
+	this.canvas = this.element.append('canvas');
+	this.context = this.canvas
+			.node().getContext('2d');
+	this.svg = this.element.append("svg");
+	this.svg.append("rect").attr("class","innerBorder");
+	this.svg.append("text").attr("class","xTitle");
+	this.svg.append("g").attr("class","yTitle").append("text").attr("class","yTitle");
+	this.svg.append("g").attr("class","innerPlot");
 
 }
 
@@ -44,9 +53,6 @@ DotPlot.prototype.setData = function(data) {
 
 	// Store data indexed by chromosome:
 	this.state.data_by_chromosome = R.groupBy(R.prop("ref"), data);
-
-	// Keys hardcoded for now, but check periodically that plot still works when these are swapped
-	this.k = { x: "ref", y: "query" };
 
 	// scales
 	this.scales = {};
@@ -83,66 +89,76 @@ DotPlot.prototype.selectQueries = function(queryIndices) {
 }
 
 DotPlot.prototype.drawStatics = function() {
+	
 	// Set up the static parts of the view that only change when width or height change, but not when zooming or changing data
-
-	// Draw initial canvas
-	this.canvas = this.element.append('canvas');
-	this.context = this.canvas
-			.attr('width', this.width)
-			.attr('height', this.height)
-			.node().getContext('2d');
-
-	var c = this.context;
-
-	// Draw outside border rectangle
-	c.setTransform(1, 0, 0, 1, 0, 0);
-	c.rect(0,0,this.width,this.height);
-	c.stroke();
+	var paddingLeft = 120;
+	var paddingBottom = 100;
+	var paddingTop = 10;
+	var paddingRight = 10;
 
 	// Inside plotting area:
-	this.layout = {
-		left: this.settings.padding.left,
-		top: 0,
-		width: this.width - this.settings.padding.left,
-		height: this.height - this.settings.padding.bottom
+	this.state.layout.inner = {
+		left: paddingLeft,
+		top: paddingTop,
+		width: this.state.layout.whole.width - paddingLeft - paddingRight,
+		height: this.state.layout.whole.height - paddingBottom - paddingTop,
 	}
 
-	
-	
-	c.setTransform(1, 0, 0, 1, this.layout.left, this.layout.top);
+	this.svg
+		.attr("width", this.state.layout.whole.width)
+		.attr("height", this.state.layout.whole.height);
 
-	////////////////////////////////////////    Borders    ////////////////////////////////////////
-	
-	// Inner plot border
-	c.rect(0,0,this.layout.width, this.layout.height);
+	this.svg.select("g.innerPlot")
+		.attr("transform", "translate(" + this.state.layout.inner.left + "," + this.state.layout.inner.top + ")");
 
-	// Background on inner plot
-	// c.fillStyle = "#ffffff"; //"#dbf0ff";
-	// c.fillRect(0,0,this.layout.width, this.layout.height);
-	
-	//////////////////////////////////////    Axis titles    //////////////////////////////////////
-	
-	// X-axis label (this.k.x is "ref" by default)
-	c.fillStyle = "#000000";
-	c.font="20px Arial";
-	c.textAlign = "center";
-	c.fillText(this.k.x, this.layout.width/2, this.config.height - 10);
+	this.canvas
+		.attr('width', this.state.layout.whole.width)
+		.attr('height', this.state.layout.whole.height);
 
-	// Y-axis label (this.k.y is "query" by default)
-	c.save()
-	c.rotate(-Math.PI/2);
-	c.textAlign = "center";
-	c.fillText(this.k.y, -this.layout.height/2, -this.layout.left + 20);
-	c.restore() // undo the rotation so we don't mess up everything else!
 
 	//////////////////////////////////////    Set up scales for plotting    //////////////////////////////////////
 
 	// Set scales with the correct inner size, but don't use them to translate, since we will be applying a translate in the draw function itself
-	this.scales.x.range([0, this.layout.width]);
-	this.scales.y.range([this.layout.height, 0]);
+	this.scales.x.range([0, this.state.layout.inner.width]);
+	this.scales.y.range([this.state.layout.inner.height, 0]);
 
+	var c = this.context;
 
+	// Draw outside border rectangle
+	// c.setTransform(1, 0, 0, 1, 0, 0);
+	// c.strokeStyle = "#f0f";
+	// c.rect(0,0,this.state.layout.whole.width,this.state.layout.whole.height);
+	// c.stroke();
 
+	////////////////////////////////////////    Borders    ////////////////////////////////////////
+	
+	// Inner plot border
+	this.svg.select("rect.innerBorder")
+		.attr("x", this.state.layout.inner.left)
+		.attr("y", this.state.layout.inner.top)
+		.attr("width", this.state.layout.inner.width)
+		.attr("height", this.state.layout.inner.height)
+		.style("stroke","red");
+
+	//////////////////////////////////////    Axis titles    //////////////////////////////////////
+	// Ref
+	this.svg.select("text.xTitle")
+		.attr("x", this.state.layout.inner.left + this.state.layout.inner.width/2)
+		.attr("y", this.state.layout.whole.height-10)
+		.style("dominant-baseline","middle")
+		.style("text-anchor","middle")
+		.style("font-size", 20)
+		.text(this.k.x);
+	
+	// Query
+	this.svg.select("g.yTitle")
+		.attr("transform", "translate(20," + this.state.layout.inner.height/2 + ")")
+		.select("text.yTitle")
+			.attr("transform", "rotate(-90)")
+			.style("dominant-baseline","middle")
+			.style("text-anchor","middle")
+			.style("font-size", 20)
+			.text(this.k.y);
 }
 
 DotPlot.prototype.drawGrid = function() {
@@ -150,41 +166,125 @@ DotPlot.prototype.drawGrid = function() {
 	var c = this.context;
 
 	// Translate everything relative to the inner plotting area
-	c.setTransform(1, 0, 0, 1, this.layout.left, this.layout.top);
-
+	c.setTransform(1, 0, 0, 1, this.state.layout.inner.left, this.state.layout.inner.top);
 	
 	/////////////////////////////////////////    Grid and axis labels    //////////////////////////////////////////
 
-	c.strokeStyle = "#AAAAAA";
-	c.fillStyle = "#000000";
-
-	// Vertical lines for sequence boundaries along the x-axis
 	const boundariesX = this.scales.x.getBoundaries();
-	c.font="10px Arial";
-	c.textAlign = "right";
-	for (var i = 0; i < boundariesX.length; i++) {
-		// Scale has already been applied inside getBoundaries()
-		c.moveTo(boundariesX[i].start,0);
-		c.lineTo(boundariesX[i].start,this.layout.height);
-		// rotated axis labels
-		c.save();
-		c.translate((boundariesX[i].start+boundariesX[i].end)/2,this.layout.height + 20);
-		c.rotate(-Math.PI/4);
-		c.fillText(boundariesX[i].name, 0, 0);
-		c.restore();
-	}
-	
-	// Horizontal lines for sequence boundaries along the y-axis
-	c.font="10px Arial";
-	c.textAlign = "right";
 	const boundariesY = this.scales.y.getBoundaries();
-	for (var i = 0; i < boundariesY.length; i++) {
-		// Scale has already been applied inside getBoundaries()
-		c.moveTo(0,boundariesY[i].start);
-		c.lineTo(this.layout.width,boundariesY[i].start);
-		c.fillText(boundariesY[i].name, -10, (boundariesY[i].start+boundariesY[i].end)/2);
-	}
-	c.stroke();	
+
+
+
+	// //////////////////////    Grid by canvas    //////////////////////
+	// c.strokeStyle = "#AAAAAA";
+	// c.fillStyle = "#000000";
+
+	// // Vertical lines for sequence boundaries along the x-axis
+	// c.font="10px Arial";
+	// c.textAlign = "right";
+	// for (var i = 0; i < boundariesX.length; i++) {
+	// 	// Scale has already been applied inside getBoundaries()
+	// 	c.moveTo(boundariesX[i].start,0);
+	// 	c.lineTo(boundariesX[i].start,this.state.layout.inner.height);
+	// 	// rotated axis labels
+	// 	c.save();
+	// 	c.translate((boundariesX[i].start+boundariesX[i].end)/2,this.state.layout.inner.height + 20);
+	// 	c.rotate(-Math.PI/4);
+	// 	c.fillText(boundariesX[i].name, 0, 0);
+	// 	c.restore();
+	// }
+	
+	// // Horizontal lines for sequence boundaries along the y-axis
+	// c.font="10px Arial";
+	// c.textAlign = "right";
+	
+	// for (var i = 0; i < boundariesY.length; i++) {
+	// 	// Scale has already been applied inside getBoundaries()
+	// 	c.moveTo(0,boundariesY[i].start);
+	// 	c.lineTo(this.state.layout.inner.width, boundariesY[i].start);
+	// 	c.fillText(boundariesY[i].name, -10, (boundariesY[i].start+boundariesY[i].end)/2);
+	// }
+	// c.stroke();	
+
+
+
+
+	//////////////////////    Grid by svg    //////////////////////
+
+	var verticalLines = this.svg.select("g.innerPlot")
+		.selectAll("line.verticalGrid").data(boundariesX);
+
+	var newVerticalLines = verticalLines.enter().append("line")
+		.attr("class","verticalGrid");
+	
+	verticalLines.merge(newVerticalLines)
+		.style("stroke","red")
+		.attr("x1", function(d) {return d.start})
+		.attr("y1", 0)
+		.attr("x2", function(d) {return d.start})
+		.attr("y2", this.state.layout.inner.height);
+
+	verticalLines.exit().remove();
+
+
+	var horizontalLines = this.svg.select("g.innerPlot")
+		.selectAll("line.horizontalGrid").data(boundariesY);
+
+	var newHorizontalLines = horizontalLines.enter().append("line")
+		.attr("class","horizontalGrid");
+
+	horizontalLines.merge(newHorizontalLines)
+		.style("stroke","red")
+		.attr("x1", 0)
+		.attr("y1", function(d) {return d.start})
+		.attr("x2", this.state.layout.inner.width)
+		.attr("y2", function(d) {return d.start});
+
+	horizontalLines.exit().remove();
+
+
+	//////////////////////    Labels by svg    //////////////////////
+
+	// c.translate((boundariesX[i].start+boundariesX[i].end)/2,this.state.layout.inner.height + 20);
+
+	var xLabels = this.svg.select("g.innerPlot")
+		.selectAll("g.xLabels").data(boundariesX);
+
+	var newXLabels = xLabels.enter().append("g")
+		.attr("class","xLabels")
+	
+	newXLabels.append("text")
+		.style("text-anchor","end")
+		.style("font-size", 10)
+		.text("hello")
+		.attr("transform", "rotate(-45)")
+
+	var innerHeight = this.state.layout.inner.height;
+	xLabels = xLabels.merge(newXLabels)
+		.attr("transform",function(d) {return "translate(" + (d.start+d.end)/2 + "," + (innerHeight + 20) + ")"})
+	
+	xLabels.select("text").datum(function(d) {return d})
+			.text(function(d) {return d.name});
+
+	xLabels.exit().remove();
+
+
+
+	var yLabels = this.svg.select("g.innerPlot")
+		.selectAll("text.yLabels").data(boundariesY);
+
+	var newYLabels = yLabels.enter().append("text")
+		.attr("class","yLabels")
+		.style("text-anchor","end")
+		.style("font-size", 10);
+
+	yLabels.merge(newYLabels)
+		.attr("x", -10)
+		.attr("y", function(d) {return (d.start+d.end)/2})
+		.text(function(d) {return d.name});
+
+	yLabels.exit().remove();
+
 }
 
 DotPlot.prototype.drawAlignments = function() {
@@ -198,6 +298,9 @@ DotPlot.prototype.drawAlignments = function() {
 	var y = this.k.y;
 	
 	// Draw lines
+
+	c.setTransform(1, 0, 0, 1, this.state.layout.inner.left, this.state.layout.inner.top);
+
 	c.beginPath();
 	c.strokeStyle = "#000000";
 	
